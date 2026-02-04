68ba5a179ab9727c03596952a3464a5c10109c99
Priyanshu <priyanshuup04@gmail.com>
2026-02-04 18:10:44 +0530

    env file name error

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const server = http.createServer(App);
 
 // Socket Server Initialization
     initSocket(server);
-    const port = process.env.port || 5000;
+    const port = process.env.PORT || 5000;
 
-    server.listen(port, (req, res) => {
-        console.log(`Server is running on the port: ${port}`);
-    });
+   server.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running on the port: ${port}`);
+});
+
+
